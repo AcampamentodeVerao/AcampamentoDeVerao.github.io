6d9ea531e81c1f3da7381dab538c3afb892682a5
AcampamentodeVerao <acampamentodeverao2025@gmail.com>
2026-03-05 11:54:28 -0300

    alterações 2027
    Preparação para a Edição de 2027

---
+++ js/date.js
@@ -0,0 +1,9 @@
+const anoAtual = new Date().getFullYear();
+let year = anoAtual + 1
+
+document.querySelector('title').innerHTML = `Acampamento de Verão ${year}`
+document.getElementById('h1_Home').innerHTML = `Acampamento de Verão ${year}`
+document.getElementById('dataAcampamento').innerHTML = `EM BREVE`
+
+document.getElementById('h1_sobre').innerHTML = `Acampamento de Verão ${year}`
+document.getElementById('span_footer').innerHTML = `Acampamento de Verão ${year}`
