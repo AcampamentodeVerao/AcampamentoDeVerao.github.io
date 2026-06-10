e997a1d4bff41aea22ae60b34913fad4d6b04256
AcampamentodeVerao <acampamentodeverao2025@gmail.com>
2026-06-10 17:50:43 -0300

    Alteração no tittle do Site

--- js/date.js
+++ js/date.js
@@ -3,6 +3,7 @@ let year = anoAtual + 1
 
 document.querySelector('title').innerHTML = `Acampamento de Verão ${year}`
 document.getElementById('h1_Home').innerHTML = `Acampamento de Verão ${year}`
+document.getElementById('titleSite').innerHTML = `Acampamento | ${year}`
 document.getElementById('dataAcampamento').innerHTML = `EM BREVE`
 
 document.getElementById('h1_sobre').innerHTML = `Acampamento de Verão ${year}`
